e27a287adf2a3d7c74763504ebdfa31acd5bbada
MihovilLukic <131758410+MihovilLukic@users.noreply.github.com>
2023-04-29 15:16:17 +0200

    Update trci_desno_0.2.py

--- trci_desno.py
+++ trci_desno.py
@@ -8,13 +8,18 @@ pygame.init()
 
 W, H = 800, 447
 win = pygame.display.set_mode((W,H))
-pygame.display.set_caption('TRČI DESNO!')
+pygame.display.set_caption('Side Scroller')
 
-bg = pygame.image.load(os.path.join('images', 'bg.png')).convert()
+bg = pygame.image.load(os.path.join('images','bg.png')).convert()
 bgX = 0
 bgX2 = bg.get_width()
 
 clock = pygame.time.Clock()
+
+run = True
+speed = 30
+pygame.time.set_timer(USEREVENT+1, 500)
+
 
 class player(object):
     run = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(8,16)]
@@ -29,17 +34,19 @@ class player(object):
         self.jumping = False
         self.sliding = False
         self.slideCount = 0
-        self.jumpcount = 0
-        self.runcount =  0
+        self.jumpCount = 0
+        self.runCount = 0
         self.slideUp = False
-        
+
     def draw(self, win):
         if self.jumping:
-            self.y -= self.jumpList[self.jumpcountm] * 1.2
-            win.blitt (self.jump[self.jumpcount//18], (self.x,self.y))
-            self.jumpcount += 1
-            if self.jumpcount > 100:
-                self.jumpcount = 0
+            self.y -= self.jumpList[self.jumpCount] * 1.2
+            win.blit(self.jump[self.jumpCount//18], (self.x,self.y))
+            self.jumpCount += 1
+            if self.jumpCount > 108:
+                self.jumpCount = 0
+                self.jumping = False
+                self.runCount = 0
         elif self.sliding or self.slideUp:
             if self.slideCount < 20:
                 self.y += 1
@@ -50,29 +57,30 @@ class player(object):
             if self.slideCount >= 110:
                 self.slideCount = 0
                 self.slideUp = False
-                self.runcount = 0
-            win.blit(self.slide[self.slideCount//10], (self.x, self.y ))
+                self.runCount = 0
+            win.blit(self.slide[self.slideCount//10], (self.x,self.y))
             self.slideCount += 1
-        
+            
         else:
-            if self.runcount > 42:
-                self.runcount = 0
-            win.blit(self.run[self.runcount//6], (self.x, self.y))
-            self.runcount += 1
+            if self.runCount > 42:
+                self.runCount = 0
+            win.blit(self.run[self.runCount//6], (self.x,self.y))
+            self.runCount += 1
  
  
-def redraw_window():
+def redrawWindow():
     win.blit(bg, (bgX, 0))
-    win.blit(bg, (bgX, 0))
-    pygame.display.update()             
+    win.blit(bg, (bgX2, 0))
+    runner.draw(win)
+    pygame.display.update() 
+                
 
 
-run = True
-speed = 30
+runner = player(200, 313, 64, 64)
       
 while run:
-    
-    clock.tick(speed)
+    redrawWindow()
+    #clock.tick(speed)
     bgX -= 1.4
     bgX2 -= 1.4
     
@@ -81,15 +89,19 @@ while run:
         
     if bgX2 < bg.get_width() * -1:
         bgX2 = bg.get_width()
-    
+       
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-            
-            draw_window()
+            run = False         
             pygame.quit
             quit() 
+    
+        if event.type == USEREVENT+1:
+            speed += 1
             
+    clock.tick(speed)    
+    
+     
     
                    
         
